88ac723ef9993d056b92a34488f8cb436b39b867
Rithu Eswaramoorthy <rithue2000@gmail.com>
2020-02-29 12:10:22 -0800

    Update button test

--- test_buttons.py
+++ test_buttons.py
@@ -7,12 +7,18 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(14,GPIO.RISING,callback=playVideo)
 '''
+def print_pressed():
+    print("button pressed")
+
+def print_released():
+    print("button released")
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(0, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 button = Button(0)
 
-button.when_pressed = print("button pressed")
-button.when_released = print("button released")
+button.when_pressed = print_pressed
+button.when_released = print_released
 
 while True:
     pass
