247b3f7df8c0e12f0766314fb54bd3fe7cd3d64a
Rithu Eswaramoorthy <rithue2000@gmail.com>
2020-02-29 11:53:08 -0800

    Update loop in button test

--- test_buttons.py
+++ test_buttons.py
@@ -9,11 +9,6 @@ GPIO.add_event_detect(14,GPIO.RISING,callback=playVideo)
 '''
 
 button = Button(0)
-button.when_pressed = print("button pressed")
 
-def run():
-    # if GPIO.input(0) == 1:
-    #     print("Button pin 0 is off")
-    #     while GPIO.input(0) == 0:
-    #         print("Button pin 0 pressed")
-    print("button pressed")
+while True:
+    button.when_pressed = print("button pressed")
